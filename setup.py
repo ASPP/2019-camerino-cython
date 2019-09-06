c97731f195789bd0fa132aae8c2391c316d70fe8
ASPP Student <student@localhost>
2019-09-06 12:51:40 +0200

    cython exercise

--- setup.py
+++ setup.py
@@ -6,6 +6,12 @@ https://github.com/pypa/sampleproject/blob/master/setup.py
 """
 from setuptools import setup, find_packages
 
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+
+import numpy as np
+
 
 with open('README', 'r') as fh: 
     long_description = fh.read()
@@ -24,4 +30,12 @@ setup(
     install_requires=['numpy >= 1.14.0',
                       'matplotlib >= 3.0.0',
                       'pytest >= 3.0.0'],
+
+    cmdclass = {'build_ext': build_ext},
+    ext_modules = [
+        Extension("prange_demo", ["prange_demo.pyx"],
+              include_dirs=[np.get_include()],
+              extra_compile_args=['-fopenmp'],
+              extra_link_args=['-fopenmp', '-lgomp']),
+    ]
 )
